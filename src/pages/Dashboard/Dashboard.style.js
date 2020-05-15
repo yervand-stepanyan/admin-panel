3d6bdb4771333985bdb1975d38cdab61e8d36e5f
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-05-15 18:49:00 +0400

    Refactor styles

--- src/pages/Dashboard/Dashboard.style.js
+++ src/pages/Dashboard/Dashboard.style.js
@@ -1,9 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles((theme) => ({
+export const useStyles = makeStyles({
   dashboardContainer: {
     display: 'flex',
     justifyContent: 'center',
-    padding: `0 ${theme.customSpacing.base}`,
   },
-}));
+});
--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -1,9 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles((theme) => ({
+export const useStyles = makeStyles({
   homeContainer: {
     display: 'flex',
     justifyContent: 'center',
-    padding: `0 ${theme.customSpacing.base}`,
   },
-}));
+});
