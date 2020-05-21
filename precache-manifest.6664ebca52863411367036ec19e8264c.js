013f443d41690c878cf81dedcb84b75d9c84d2bb
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-05-21 15:26:38 +0400

    Updates

---
+++ precache-manifest.6664ebca52863411367036ec19e8264c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2c58872c4d84e64789bd5f6385802b",
+    "url": "/admin-panel/index.html"
+  },
+  {
+    "revision": "4b0f73056f6f00397a32",
+    "url": "/admin-panel/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "792386e9a3473d476959",
+    "url": "/admin-panel/static/js/2.578b30e2.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/admin-panel/static/js/2.578b30e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0f73056f6f00397a32",
+    "url": "/admin-panel/static/js/main.5123f995.chunk.js"
+  },
+  {
+    "revision": "a08add4cae019a4d823d",
+    "url": "/admin-panel/static/js/runtime-main.30cf401c.js"
+  }
+]);
