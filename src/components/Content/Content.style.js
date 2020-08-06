c2eb9b94654dbfc30aad6c34ee9317cf60aa74e9
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-08-06 12:01:27 +0200

    Refactor content padding-bottom in Content.style.js

--- src/components/Content/Content.style.js
+++ src/components/Content/Content.style.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles((theme) => ({
   contentContainer: {
     flexGrow: 1,
-    padding: `${theme.customSpacing.xxxLarge} ${theme.customSpacing.base} ${theme.customSpacing.base}`,
+    padding: `${theme.customSpacing.xxxLarge} ${theme.customSpacing.base}`,
     width: `calc(100% - ${theme.drawer.drawerWidth}px)`,
   },
 }));
